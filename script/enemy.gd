4784c2c3bb5a1a9694b78f0131df8ad38cb1aa56
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2024-02-26 23:53:57 -0600

    Remove player reference

--- script/enemy.gd
+++ script/enemy.gd
@@ -11,7 +11,6 @@ var direction : Vector2
 #direction and animation to be updated throughout game state
 var new_direction = Vector2(0,1) #only move one spaces
 
-@onready var player = get_tree().root.get_node("Gameboard/MyDude")
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 @onready var health_bar: MTDBar = $HealthBar
 
