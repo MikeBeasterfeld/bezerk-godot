5bde61de8a99b4695f3e0a1848b7b92ce8614475
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2024-03-27 07:08:31 -0500

    Add a crosshair

--- player/my_dude.gd
+++ player/my_dude.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var bullet_scene = load("res://projectile/bullet.tscn")
 @onready var health_bar: MTDBar = $HealthBar
+@onready var crosshair: Sprite2D = $Crosshair
 
 var last_shot: float = Time.get_unix_time_from_system()
 
@@ -20,12 +21,19 @@ func _physics_process(_delta: float) -> void:
 	velocity = move_speed * move_direction.normalized()
 
 	var current_time: float = Time.get_unix_time_from_system()
+
+	var shoot_direction = Input.get_vector("ui_shoot_left", "ui_shoot_right", "ui_shoot_up", "ui_shoot_down").normalized()
+	
+	if (shoot_direction != Vector2(0,0)):
+		crosshair.visible = true
+		crosshair.position = shoot_direction * Vector2(150, 150)
+	else:
+		crosshair.visible = false
 	
 	if(last_shot + shoot_speed < current_time):
-		var shoot_direction = Input.get_vector("ui_shoot_left", "ui_shoot_right", "ui_shoot_up", "ui_shoot_down")
 		
 		if(abs(shoot_direction.x) > 0 or abs(shoot_direction.y) > 0):
-			print(shoot_direction)
+			print("shoot direction ", shoot_direction)
 			print(abs(shoot_direction.x) - abs(shoot_direction.y))
 			var instance = bullet_scene.instantiate()
 			instance.position = position
