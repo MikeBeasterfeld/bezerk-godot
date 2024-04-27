6b0a2b50b69839912307e164a6d501d87602cd6d
Michael Beasterfeld <michael.beasterfeld@integral.io>
2024-04-27 18:10:47 -0500

    re-bake navigation on timer to update for destoyed walls

--- level/gameboard.gd
+++ level/gameboard.gd
@@ -6,12 +6,14 @@ var enemies = [preload("res://enemy/boom_bot.tscn"), preload("res://enemy/shooty
 @onready var my_dude: CharacterBody2D = $MyDude
 @onready var timer: Timer = $Timer
 @onready var fps: Label = $FPS
+@onready var navigation_region_2d = $NavigationRegion2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	fps.text = str(Engine.get_frames_per_second()) + " FPS"
 
 func _on_timer_timeout() -> void:
+	navigation_region_2d.bake_navigation_polygon()
 	
 	var new_mob = (enemies.pick_random()).instantiate()
 	%SpawnFollow.progress_ratio = randf()
